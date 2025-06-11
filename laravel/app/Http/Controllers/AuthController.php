3498a5f0a9cd0a153ae769270bce406f3844f12c
mateusalecrin <mateus.alecrin@hotmail.com>
2025-06-10 22:16:38 -0300

    new: authentication, users roles

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -4,11 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Http\Requests\AuthRequest;
+use App\Repositories\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function __construct(private UserRepository $userRepository)
     {
 
+    }
+
+    public function login(AuthRequest $request)
+    {
+        $user = $this->userRepository->findByEmail($request->email);
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.']
+            ]);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json(['token' => $token]);
+    }
+
+    public function profile()
+    {
+        $user = Auth::user();
+        $user->load('roles');
+        return new UserResource($user);
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,20 +4,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
-
-Route::apiResource('/roles', RoleController::class);
-
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
-Route::get('/', fn () => response()->json(['message' => 'ok']));
+use App\Http\Controllers\RoleUserController;
 
 Route::prefix('auth')->group(function () {
+    Route::get('/profile', [AuthController::class, 'profile'])->name('auth.profile')->middleware('auth:sanctum');
     Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('auth.logout');
-    Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum')->name('auth.user');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/roles', RoleController::class);
+
+    Route::get('/users/{user}/roles', [RoleUserController::class, 'index'])->name('users.roles.index');
+    Route::put('/users/{user}/roles', [RoleUserController::class, 'sync'])->name('users.roles.sync');
+
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+});
+
+Route::get('/', fn () => response()->json(['message' => 'ok']));
